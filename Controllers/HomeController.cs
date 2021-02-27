a34c44daf2ed6351ae0fd3efc5fad9f7fc2f3694
Avinash Deokate <avinashdeokate11@gmail.com>
2021-02-27 10:51:38 +0530

    Added About and contact page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,14 @@ namespace URYVAP.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
